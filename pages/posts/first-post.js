304ba870cde362e371def83371c8f3a4b8c57257
Sin <xinguihuan@sMikehs-MacBook-Pro.local>
2024-04-15 15:50:00 +0900

    Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    
+    <Layout>
+     <Head>
+        <title>Love professor Youngjoon</title>
+       
+      </Head>
+      <h1>Hi professor my name is kyuhwan you can call me Mike or Micheal, I'll do my best to listen to the lecture!!!
+      </h1>
+   
+      
+    </Layout>
+    
+  );
+}
